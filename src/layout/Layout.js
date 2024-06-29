f5d94cb3e7bddf9dfe629480066e31ea202fc282
crock <143845383+chimfwembeC@users.noreply.github.com>
2024-06-29 16:30:36 +0200

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,5 +1,5 @@
 // layout/Layout.js
-import React from "react";
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
@@ -8,6 +8,13 @@ import FooterComponent from './FooterComponent';
 
 import { Outlet } from "react-router-dom";
 const Layout = () => (
+const AdSenseAd = () => {
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
   <div>
     <header>
       <nav>
@@ -16,6 +23,13 @@ const Layout = () => (
     </header>
     <main>
       <Outlet />
+<ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-6221660864872022" // Replace with your client ID
+      data-ad-slot="1234567890" // Replace with your ad slot ID
+      data-ad-format="auto"
+    />
     </main>
   <FooterComponent />
   </div>
